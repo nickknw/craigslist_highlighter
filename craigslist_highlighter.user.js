cd53fc818792bf5b6b323cbfa71e65b9fc104c5b
Nick <nickknw@gmail.com>
2011-02-07 20:46:33 -0800

    Large amount of progress. Finally in working state
    Haven't tested throroughly, but script should work in all
    'best case scenarios' as well as avoiding most obvious pitfalls.
    
    Known Todos at the moment: tabbing support, using keyboard to
    toggle the on/off radio buttons

--- craigslist_highlighter.user.js
+++ craigslist_highlighter.user.js
@@ -10,26 +10,29 @@
 
 var MaxPrice = 875;
 
-var animalGoodPhrases = new Array("% negotiable", "% considered", "% permitted", "% ok", "%s ok", "%s are OK - purrr", "maybe a %", "%s are OK - wooof");
-var animalBadPhrases = new Array("no %", "no %s", "%s not permitted", "no %s permitted");
+var positiveMatchColour = "#69EF68";
+var negativeMatchColour = "#EFB3B3";
 
-var petGoodPhrases = replaceInArray(animalGoodPhrases, "pet");
-var petBadPhrases = replaceInArray(animalBadPhrases, "pet")
+var goodPhrases = new Array("% negotiable", "% considered", "% permitted", "% ok", "%s ok", "%s are OK - purrr", "maybe a %", "%s are OK - wooof");
+var badPhrases = new Array("no %\b", "no %s", "%s not permitted", "no %s permitted");
 
-var catGoodPhrases = Array.concat(replaceInArray(animalGoodPhrases, "cat"), petGoodPhrases);
-var catBadPhrases = Array.concat(replaceInArray(animalBadPhrases, "cat"), petBadPhrases);
+var petGoodPhrases = replaceInArray(goodPhrases, "pet");
+var petBadPhrases = replaceInArray(badPhrases, "pet")
 
-var dogGoodPhrases = Array.concat(replaceInArray(animalGoodPhrases, "dog"), petGoodPhrases);
-var dogBadPhrases = Array.concat(replaceInArray(animalBadPhrases, "dog"), petBadPhrases);
+var catGoodPhrases = replaceInArray(goodPhrases, "cat");
+var catBadPhrases = replaceInArray(badPhrases, "cat");
 
-var dishwasherGood = new Array("dishwasher");
-var dishwasherBad = new Array("no dishwasher");
+var dogGoodPhrases = replaceInArray(goodPhrases, "dog");
+var dogBadPhrases = replaceInArray(badPhrases, "dog");
 
-var fireplaceGood = new Array("fireplace");
-var fireplaceBad = new Array("no fireplace");
+var dishwasherGoodPhrases = new Array("dishwasher");
+var dishwasherBadPhrases = new Array("no dishwasher");
 
-var balconyGood = new Array("balcony");
-var balconyBad = new Array("no balcony");
+var fireplaceGoodPhrases = new Array("fireplace");
+var fireplaceBadPhrases = new Array("no fireplace");
+
+var balconyGoodPhrases = new Array("balcony");
+var balconyBadPhrases = new Array("no balcony");
 
 var timesAvailable = new Array("January", "Jan", "February", "Feb", "March", "Mar", "April", "Apr", "June", "Jun", "July", "Jul", "August", "Aug", "September", "Sept", "October", "Oct", "November", "Nov", "December", "Dec", "immediately");
 
@@ -60,33 +63,60 @@ var user_undesirablePhrases;
 
 $(document).ready(function() {
 
-    craigslistHighlighterIsOn = GM_getValue('user_isHighlightingOn', true)
-    $("#searchtable > tbody").append(onOffSwitch());
+    craigslistHighlighterIsOn = GM_getValue('user_highlighterIsOn', true)
 
-    if(!craigslistHighlighterIsOn) {
-        return;
+    if($("#searchtable").length > 0) {
+
+        // on/off switch with listener 
+        $("#searchtable > tbody").append(onOffSwitch(craigslistHighlighterIsOn));
+        document.getElementById("powerOn").addEventListener('click', function(){ toggleCraigslistHighlighter(craigslistHighlighterIsOn, "on");}, false);
+        document.getElementById("powerOff").addEventListener('click', function(){ toggleCraigslistHighlighter(craigslistHighlighterIsOn, "off");}, false);
+        
+
+        if(!craigslistHighlighterIsOn) {
+            return;
+        }
+
+        // attach listener that saves data to Search button 
+        document.getElementById("searchform").addEventListener("submit", scrapeAndSaveUserData, false);
+
+        // Insert extra controls
+        $("#searchtable > tbody").append(preferredLocationsControl());
+        $("#searchtable > tbody").append(preferredPhrasesControl());
+        $("#searchtable > tbody").append(preferredFeaturesCheckboxes());
+
+        loadUserData();
+        insertUserData();
+
+        processListingsPage();
     }
+    else {
+        if(!craigslistHighlighterIsOn) {
+            return; 
+        }
 
-    // attach listener that saves data to Search button 
-    document.getElementById("searchform").addEventListener("submit", scrapeAndSaveUserData, false);
-
-    // Insert extra controls
-    $("#searchtable > tbody").append(preferredLocationsControl());
-    $("#searchtable > tbody").append(preferredPhrasesControl());
-    $("#searchtable > tbody").append(preferredFeaturesCheckboxes());
-
-    loadAndInsertUserData();
-
-    processListingsPage();
-
-    processIndividualPage();
+        processIndividualPage();
+    }
 });
 
 // }}}
 
 // Helper Functions {{{
 
-function loadAndInsertUserData() {
+function toggleCraigslistHighlighter(highlighterIsOn, radioButtonClicked) {
+
+    if ((highlighterIsOn && radioButtonClicked == "on") 
+    || (!highlighterIsOn && radioButtonClicked == "off") ) {
+        return;
+    }
+
+    GM_setValue('user_highlighterIsOn', !highlighterIsOn);
+
+    // page refresh or form submit
+    document.getElementById("searchform").submit();
+}
+
+function loadUserData() {
 
     user_rentMin = GM_getValue('user_rentMin', null);
     user_rentMax = GM_getValue('user_rentMax', null);
@@ -102,36 +132,71 @@ function loadAndInsertUserData() {
     user_desirablePhrases = GM_getValue('user_desirablePhrases', null);
     user_undesirablePhrases = GM_getValue('user_undesirablePhrases', null);
 
+    if(user_goodLocations == null || user_goodLocations == '') {
+        user_goodLocations = [];
+    }
+    else {
+        user_goodLocations = String.split(user_goodLocations, ",");
+    }
+
+    if(user_badLocations == null || user_badLocations == '') {
+        user_badLocations = [];
+    }
+    else {
+        user_badLocations = String.split(user_badLocations, ",");
+    }
+
+    if(user_desirablePhrases == null || user_desirablePhrases == '') {
+        user_desirablePhrases = [];
+    }
+    else {
+        user_desirablePhrases = String.split(user_desirablePhrases, ",");
+    }
+
+    if(user_undesirablePhrases == null || user_undesirablePhrases == '') {
+        user_undesirablePhrases = [];
+    }
+    else {
+        user_undesirablePhrases = String.split(user_undesirablePhrases, ",");
+    }
+}
+
+function insertUserData() {
+
     focusAndSetValue(".min", user_rentMin);
     focusAndSetValue(".max", user_rentMax);
 
-    $("input[name='addTwo']").val(user_catsOn);
-    $("input[name='addThree']").val(user_dogsOn);
-    $("#chkFireplace").val(user_fireplaceOn);
-    $("#chkBalcony").val(user_balconyOn);
-    $("#chkDishwasher").val(user_dishwasherOn);
+    $("#chkCats").attr('checked', user_catsOn);
+    $("#chkDogs").attr('checked', user_dogsOn);
+    $("#chkFireplace").attr('checked', user_fireplaceOn);
+    $("#chkBalcony").attr('checked', user_balconyOn);
+    $("#chkDishwasher").attr('checked', user_dishwasherOn);
 
-    focusAndSetValue(".goodLocations", user_goodLocations);
-    focusAndSetValue(".badLocations", user_badLocations);
-    focusAndSetValue(".desirablePhrases", user_desirablePhrases);
-    focusAndSetValue(".undesirablePhrases", user_undesirablePhrases);
+    focusAndSetValue(".goodLocations", Array.join(user_goodLocations));
+    focusAndSetValue(".badLocations", Array.join(user_badLocations));
+    focusAndSetValue(".desirablePhrases", Array.join(user_desirablePhrases));
+    focusAndSetValue(".undesirablePhrases", Array.join(user_undesirablePhrases));
 }
 
+// these should always be done as a pair, one after another
 function scrapeAndSaveUserData() {
 
+    // scrape
     user_rentMin = focusAndGetValue(".min");
     user_rentMax = focusAndGetValue(".max");
 
-    user_catsOn = $("input[name='addTwo']").val();
-    user_dogsOn = $("input[name='addThree']").val();
-    user_fireplaceOn = $("#chkFireplace").val();
-    user_balconyOn = $("#chkBalcony").val();
-    user_dishwasherOn = $("#chkDishwasher").val();
+    user_catsOn = $("#chkCats").attr('checked');
+    user_dogsOn = $("#chkDogs").attr('checked');
+    user_fireplaceOn = $("#chkFireplace").attr('checked');
+    user_balconyOn = $("#chkBalcony").attr('checked');
+    user_dishwasherOn = $("#chkDishwasher").attr('checked');
+
     user_goodLocations = focusAndGetValue(".goodLocations");
     user_badLocations = focusAndGetValue(".badLocations");
     user_desirablePhrases = focusAndGetValue(".desirablePhrases");
     user_undesirablePhrases = focusAndGetValue(".undesirablePhrases");
 
+    // save
     GM_setValue('user_rentMin', user_rentMin);
     GM_setValue('user_rentMax', user_rentMax);
 
@@ -177,11 +242,11 @@ function processListingsPage() {
 
         //is this listing in our price range?
 	var price = extractPrice(text.value);
-        if (price > MaxPrice) {
+        if (user_rentMax != null && price > user_rentMax) {
             return;
         }
 
-        //follows the listing link and retrieves the 'full listing' web page
+        // follows the listing link and retrieves the 'full listing' web page
         var link = listing.children("a").attr("href");  
         if(link == null) {
             return;
@@ -189,39 +254,32 @@ function processListingsPage() {
 
         $.get(link, function(data, textStatus) {
 
-            //list of words found in the full listing page
+            // list of words found in the full listing page
             var details = " <font size='-1'> <strong> ";         
 
-            //regex that will find the userbody tag + contents
+            // regex that will find the userbody tag + contents
             var regex = /<div id="userbody">([\s\S]*)<.div>/gi;  
 
-            //IMPORTANT: without this, the regex will fail every second 
-            //time it executes because it is saving position
+            // IMPORTANT: without this, the regex will fail every second 
+            // time it executes because it is saving position
             regex.lastIndex = 0;                                 
 
-            //extract the 'userbody' tag + contents
+            // extract the 'userbody' tag + contents
             var userbody = regex.exec(data)[0];                  
 
-            //find and extract desired words from userbody, and wrap them in the appropriate colours
-            var termsFound = "";
-            termsFound += extractWords(userbody, user_goodLocations, "#69EF68");
-            termsFound += extractWords(userbody, user_desirablePhrases, "#69EF68");
-            termsFound += extractWords(userbody, user_badLocations, "#EFB3B3");
-            termsFound += extractWords(userbody, user_undesirablePhrases, "#EFB3B3");
-            termsFound += extractWords(userbody, timesAvailable, "#BBBBFF");
-            if(termsFound != "") { termsFound = "Terms Found: " + termsFound; }
-
+            var termsFound = extractAllTermsFromPage(userbody);
+            
             details += termsFound;
             details += "</strong></font>";
 
-            //add the desired listing details to the listings page
+            // add the desired listing details to the listings page
             listing.append(details);      
 
-            //get the updated listing text
+            // get the updated listing text
             text.value = listing.html();  
 
-            //give the listing the correct background colour based on location
-            if(highlightWords(text, user_goodLocations, "#69EF68")) {
+            // give the listing the correct background colour based on location
+            if(highlightWords(text, user_goodLocations, positiveMatchColour)) {
                 listing.css("background-color", "#AAFFAA");
             }
             if(highlightWords(text, user_badLocations, "#EFB3B3")) {
@@ -230,18 +288,19 @@ function processListingsPage() {
             
             var price = extractPrice(text.value);
             
-            color = price - 675;
+            if(user_rentMin != null) {
+                color = price - user_rentMin;
 
-            color = color / 22;
-            color = Math.floor(color);
-            
-            if(color < 0) { color = 0; }
-            if(color > 9) { color = 9; }
+                color = color / 22;
+                color = Math.floor(color);
+                
+                if(color < 0) { color = 0; }
+                if(color > 9) { color = 9; }
 
-            colorInHex = "#" + color + color + color + color + color + color;
+                colorInHex = "#" + color + color + color + color + color + color;
 
-            //console.log(colorInHex);
-            highlightDollarAmount(text, price, colorInHex);
+                highlightDollarAmount(text, price, colorInHex);
+            }
 
             listing.html(text.value);    //save the new listing text back into the listing
 
@@ -249,7 +308,58 @@ function processListingsPage() {
     });
 }
 
+function extractAllTermsFromPage(userbody) {
+    // find and extract desired words from userbody, and wrap them in the appropriate colours
+    // **when optimizing, consider conglomerating all good arrays into one and bad arrays into one,
+    // then going through the body text less times. Might help?
+    var termsFound = "";
+    termsFound += extractWords(userbody, user_goodLocations, positiveMatchColour);
+    termsFound += extractWords(userbody, user_desirablePhrases, positiveMatchColour);
+    termsFound += extractWords(userbody, user_badLocations, negativeMatchColour);
+    termsFound += extractWords(userbody, user_undesirablePhrases, negativeMatchColour);
+
+
+    // time to check for all the checkboxes
+    if(user_catsOn || user_dogsOn)
+    {
+        termsFound += extractWords(userbody, petGoodPhrases, positiveMatchColour);
+        termsFound += extractWords(userbody, petBadPhrases, negativeMatchColour);
+
+        if(user_catsOn) {
+            termsFound += extractWords(userbody, catGoodPhrases, positiveMatchColour);
+            termsFound += extractWords(userbody, catBadPhrases, negativeMatchColour);
+        }
+        if(user_dogsOn) {
+            termsFound += extractWords(userbody, dogGoodPhrases, positiveMatchColour);
+            termsFound += extractWords(userbody, dogBadPhrases, negativeMatchColour);
+        }
+    }
+
+    if(user_balconyOn) {
+        termsFound += extractWords(userbody, balconyGoodPhrases, positiveMatchColour);
+        termsFound += extractWords(userbody, balconyBadPhrases, negativeMatchColour);
+    }
+
+    if(user_fireplaceOn) {
+        termsFound += extractWords(userbody, fireplaceGoodPhrases, positiveMatchColour);
+        termsFound += extractWords(userbody, fireplaceBadPhrases, negativeMatchColour);
+    }
+
+    if(user_dishwasherOn) {
+        termsFound += extractWords(userbody, dishwasherGoodPhrases, positiveMatchColour);
+        termsFound += extractWords(userbody, dishwasherBadPhrases, negativeMatchColour);
+    }
+
+    termsFound += extractWords(userbody, timesAvailable, "#BBBBFF");
+    if(termsFound != "") { termsFound = "Terms Found: " + termsFound; }
+
+    return termsFound;
+}
+
 function processIndividualPage() {
+
+    loadUserData();
+
     //in the body of text and the title on each individual page
     $("#userbody, h2").each(function() {
         var text = new Object();
@@ -260,6 +370,10 @@ function processIndividualPage() {
         highlightWords(text, user_badLocations, "#FFAAAA");
         highlightWords(text, user_undesirablePhrases, "#FFAAAA");
         highlightWords(text, timesAvailable, "#BBBBFF");
+
+        if(user_catsOn) {
+            highlightWords(text, catGoodPhrases, positiveMatchColour);
+        }
 
         $(this).html(text.value);
     });
@@ -356,24 +470,35 @@ function preferredPhrasesControl() {
 
 function preferredFeaturesCheckboxes() {
     return "<tr>" +
-        "<td width='1' align='right'>features:</td>" +
+        "<td width='1' align='right'> </td>" +
         "<td>" +
         "<label style='padding-right:3px;'><input type='checkbox' value='fireplace' id='chkFireplace'>fireplace</label>" +
         "<label style='padding-right:3px;'><input type='checkbox' value='balcony' id='chkBalcony'>balcony</label>" +
         "<label style='padding-right:3px;'><input type='checkbox' value='dishwasher' id='chkDishwasher'>dishwasher</label>" + 
+        "<label style='padding-right:3px;'><input type='checkbox' value='cats' id='chkCats'>cats</label>" + 
+        "<label style='padding-right:3px;'><input type='checkbox' value='dogs' id='chkDogs'>dogs</label>" + 
         "</td>" +
         "</tr>";
 }
 
-function onOffSwitch() {
-    return "<tr>" +
+function onOffSwitch(highlighterIsOn) {
+    onOffSwitchHtml = "<tr>" +
         "<td width='1' align='right'>craigslist highlighter:</td>" +
-        "<td><select><option value='on'>On</option><option value='off'>Off</option></select></td>" +
-        "</tr";
+        "<td><span><label><input type='radio' id='powerOn' name='onoff' value='on' ";
+    if(highlighterIsOn) {
+        onOffSwitchHtml += "checked='true'";
+    }
+    onOffSwitchHtml += " >On</label><label><input type='radio' id='powerOff' name='onoff' value='off' ";
+    if(!highlighterIsOn) {
+        onOffSwitchHtml += "checked='true'";
+    }
+    onOffSwitchHtml += " >Off</label></span></td></tr";
+
+    return onOffSwitchHtml;
 }
 
 function replaceInArray(array, wordToInsert) {
-    return $.map(array, function(phrase) { phrase.replace(/%/g, wordToInsert) });
+    return $.map(array, function(phrase, i) { return phrase.replace(/%/, wordToInsert) });
 }
 
 // }}}
